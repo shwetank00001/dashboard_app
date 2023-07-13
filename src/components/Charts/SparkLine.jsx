fe8663128091173b9fa2cf89e9f86daa871973b3
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-07-13 18:03:59 +0530

    fixed line

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -5,24 +5,24 @@ import { Format } from '@syncfusion/ej2/spreadsheet'
 const SparkLine = ({currentColor,id, type, color,height,width,data}) => {
   return (
     <SparklineComponent
-    id={id}
-    height={height}
-    width={width}
-    lineWidth={1}
-    valueType='Numeric'
-    fill={color}
-    border={{color:currentColor, width:2}}
-    dataSource={data}
-    xName='x'
-    yName='y'
-    type={type}
-    tooltipSettings={{
-      visible:true,
-      format:'${x}: data ${y}',
-      trackLineSettings:{
-        visible:true
-      }
-    }}
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType='Numeric'
+      fill={color}
+      border={{color:currentColor, width:2}}
+      dataSource={data}
+      xName='x'
+      yName='y'
+      type={type}
+      tooltipSettings={{
+        visible:true,
+        format:'${x}: data ${y}',
+        trackLineSettings:{
+          visible:true
+        }
+      }}
     >
 
       <Inject services ={[SparklineTooltip]} />
