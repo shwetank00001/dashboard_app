640574cfd02c3b5f15aaf55447d962475a9360ab
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-07-13 17:57:19 +0530

    line not wokring F

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
+import { Format } from '@syncfusion/ej2/spreadsheet'
 
-const SparkLine = () => {
+const SparkLine = ({currentColor,id, type, color,height,width,data}) => {
   return (
-    <div>SparkLine</div>
+    <SparklineComponent
+    id={id}
+    height={height}
+    width={width}
+    lineWidth={1}
+    valueType='Numeric'
+    fill={color}
+    border={{color:currentColor, width:2}}
+    dataSource={data}
+    xName='x'
+    yName='y'
+    type={type}
+    tooltipSettings={{
+      visible:true,
+      format:'${x}: data ${y}',
+      trackLineSettings:{
+        visible:true
+      }
+    }}
+    >
+
+      <Inject services ={[SparklineTooltip]} />
+    </SparklineComponent>
   )
 }
 
